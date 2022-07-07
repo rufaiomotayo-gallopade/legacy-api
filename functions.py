ae0fd41d7987381a9519933b78b7a9605e2ede6d
rufaiomotayo-gallopade <107952295+rufaiomotayo-gallopade@users.noreply.github.com>
2022-07-07 09:48:00 -0400

    Added more comments & print statements to functions.py

--- functions.py
+++ functions.py
@@ -10,68 +10,11 @@ load_dotenv()
 API_KEY = os.getenv("API_KEY")
 client = hubspot.Client.create(api_key=API_KEY)
 
-def get_company_info(company_id):
-    try:
-        api_response = client.crm.companies.basic_api.get_by_id(
-            company_id=company_id, 
-            archived=False
-            )
-        get_company_name(api_response)
-    except ApiException as e:
-        print("Exception when calling basic_api->get_by_id: %s\n" % e)
-
 def list_companies():
-    global company_list 
     company_list = data_to_dict()    
     for key, value in (company_list).items():
         print(key, ' - ', value)
    
-def get_company_name(api_response):
-    #global duplicates
-    count=1
-    all_companies = {
-
-    }
-
-    formatted_api_response = pformat(api_response)
-    try:
-        for x in range(100):
-                l = formatted_api_response.find("'id': '") # some number where 'id': ' is located in api response
-                response_snippet = formatted_api_response[l:formatted_api_response.find(",",l)] #snippet of api response from where it says 'id' to end of id number
-                id_start = response_snippet.find(" ",)+2
-                id_end = len(response_snippet)-1
-                company_id = response_snippet[id_start:id_end] # filters thu api response to get id number
-
-                company_name = get_name(company_id) # calls function that uses api and id to get company name
-                # line = company_name + "-" + str(company_id)
-                # with open("data.txt", "r+") as data:
-                #     if company_name not in data:
-                # # Write it; assumes file ends in "\n" already
-                #         data.write(company_name + "-" + str(company_id))
-                #         all_companies.update({company_name : company_id})
-                #     else:
-                #         print("NAME IN DATATATATATATAT")
-                if company_name in all_companies: # if the company already exists in dictionary . .. 
-                    #duplicates.append(company_name) # adds compamny name to list of duplicates
-                    #print("B4: ", all_companies.get(company_name))
-                    curr_val = all_companies.get(company_name) # stores current value of companyname to a variable
-                    if isinstance(curr_val, list): # checks if value at company_name is already a list
-                        curr_val.append(company_id) # if already a list it appends id
-                        all_companies.update({company_name : curr_val}) # updates the dictionary with appended list
-                    else:  #if value at company name is not already list then will make into a list
-                        array =[] # initializing list
-                        array.append(curr_val) # adds current value in dictionary
-                        array.append(company_id) # adds new id
-                        all_companies.update({company_name : array}) #updates dictionary with new list of ids            
-                else: # if not in dictionary...
-                    all_companies.update({company_name : company_id}) #adds to dictionary
-                #data.close
-                formatted_api_response = formatted_api_response[formatted_api_response.find("properties_with_history")+1:] # moves to next item in json response               
-    except:
-        print("ERROR")
-    
-    return all_companies
-
 def get_name(id):
     url = "https://api.hubapi.com/companies/v2/companies/"+ str(id)
     querystring = {"hapikey":API_KEY}
@@ -81,8 +24,7 @@ def get_name(id):
     response = requests.get(url=url, params = querystring)
     response = response.text # makes response searchable
     x = response.find('"name":{"value":"') #looks for name in response and finds value it starts
-    y = x+150 # arbitrary number
-    snippet = response[x:y] # encompasses area of response where name should be located
+    snippet = response[x:(x+150)] # encompasses area of response where name should be located
     company_name = snippet[17:snippet.find('","')] # filters thru snippet to get company name
     return company_name
 
@@ -91,8 +33,7 @@ def load_to_text(companies):
         for company,id in companies.items():
             line = company + '-' + str(id) + '\n'
             print(line)
-            data.write(line)
-    
+            data.write(line)  
     print("Data loaded succesfully")
     data.close
 
@@ -104,24 +45,31 @@ def make_parent(parent_id, child_id):
             to_object_id = child_id, 
             association_type = "13"
             )
-        print("SUCCESS")
+        print("SUCCESS: ", parent_id, "is now parent to", child_id)
     except ApiException as e:
         print("Exception when calling associations_api->create: %s\n" % e)
 
 def make_parents():
-    global company_list 
     company_list = data_to_dict()
     failed = []
-    open('failed.txt', 'w').close()
-    print("Attempting to make parent associations...")
+    count = 0
+    open('failed.txt', 'w').close() # supposed to clear text in failed.txt before starting
+    print("Beginning to make parent associations")
     with open("associations.txt", 'r') as infile:
         for line in infile:
+            count = count + 1
             tokens = line.strip().split('\t')
             child = tokens[0]
-            parent = tokens[1]
+            try:
+                parent = tokens[1] # if there is a blank entery this will not work which is why its in try except 
+                print("Attempting making association (", count, "/", len(company_list), ")")
+            except:
+                print("ERROR: There is probably a blank entry somewhere in all_companies.txt")
             if company_list.get(parent) == None: # if parent company is not in dictionary then ...
+                print("ERROR: Parent company not found in list of companies")
                 failed.append(parent + " failed to become parent of " + child) # appends failure message to failed array     
             elif company_list.get(child) == None: # if child company is not in dictionary then ...
+                print("ERROR: Child company not found in list of companies")
                 failed.append(child + " failed to become child of " + parent) # appends failure message to failed array
             elif (isinstance(company_list.get(parent),list)) and isinstance(company_list.get(child),list): # if parent and children are both lists
                 for parent_id in company_list.get(parent):
@@ -152,7 +100,7 @@ def data_to_dict(): #takes data from txt file and returns it in a dictionary
     with open("all_companies.txt", 'r') as infile:
         for line in infile:
             tokens = line.strip().split('\t')
-            print(tokens)
+            #print(tokens)
             company_name = tokens[1]
             #print("Company is: ",company_name)
             company_id = tokens[0]
--- legacy_api.py
+++ legacy_api.py
@@ -1,4 +1,3 @@
 import functions
 
-#functions.list_companies()
 functions.make_parents()
